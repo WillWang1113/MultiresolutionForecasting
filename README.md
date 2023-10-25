c627423358a0206d2cbd2e4210e48bcb13fb5cc9
WillWang1113 <willwang1113@163.com>
2023-10-25 23:21:10 +0800

    final

--- README.md
+++ README.md
@@ -2,24 +2,36 @@
 
 Codes for paper "Seamless and multi-resolution energy forecasting". 
 
+Authors: Chenxi Wang, Pierre Pinson, Yi Wang
+
 
 ## Requirements
+Python version: 3.8.10
+
 The must-have packages can be installed by running
 ```
 pip install requirements.txt
 ```
 
 ## Experiments
+### Reproduce
 To reconstruct the same experiments in the paper, please run
 ```
 bash run_experiments.sh
 ```
+Note: There is no multi-GPU/parallelling training in our codes. 
+
 The results(models) and logs will be saved into ```results/``` and ```logs/```. 
 
 Then, go into ```00-post_analysis.ipynb``` for post analysis, including the post-coordination for benchmarks and the comparison of the total consistency error. Please replace the variable ```pth``` in the notebook.
 
 If you also want to have the same figures in the papaer, please refer to ```01-plot.ipynb```.
 
+### Example
+An example of seamless multi-resolution forecasts on wind power from Hierarchical Laplace(HL) and benchmarks.
 
-## Acknowlegde
+![image info](./savings/display.png)
+
+
+## Acknowledge
 Package ```torchlapalace/``` is modified based on the open code of [Neural Laplace](https://github.com/samholt/NeuralLaplace)
--- run_experiments.sh
+++ run_experiments.sh
@@ -4,11 +4,11 @@ fcst_steps=288
 observe_steps=$((2 * fcst_steps))
 window_width=$((fcst_steps + observe_steps))
 
-# python example.py -d nrel --add_external_feature --observe_steps $observe_steps --window_width $window_width --latent_dim 16 --avg_terms_list 12 3 1 --gpu 3 --encoder rnn --pass_raw --shared_encoder --run_times 20
-# python example.py -d mfred --add_external_feature --observe_steps $observe_steps --window_width $window_width --latent_dim 16 --avg_terms_list 12 3 1 --gpu 3 --encoder dnn --pass_raw --shared_encoder --run_times 20
+python example.py -d nrel --add_external_feature --observe_steps $observe_steps --window_width $window_width --latent_dim 16 --avg_terms_list 12 3 1 --gpu 0 --encoder rnn --pass_raw --shared_encoder --run_times 20
+python example.py -d mfred --add_external_feature --observe_steps $observe_steps --window_width $window_width --latent_dim 16 --avg_terms_list 12 3 1 --gpu 0 --encoder dnn --pass_raw --shared_encoder --run_times 20
 
-# python example_benchmarks.py -d nrel --add_external_feature --observe_steps $observe_steps --window_width $window_width --gpu 3 --avg_terms_list 12 3 1 --run_times 20 --persistence naive 
-python example_benchmarks.py -d mfred --add_external_feature --observe_steps $observe_steps --window_width $window_width --gpu 3 --avg_terms_list 12 3 1 --run_times 20 --persistence loop 
+python example_benchmarks.py -d nrel --add_external_feature --observe_steps $observe_steps --window_width $window_width --gpu 0 --avg_terms_list 12 3 1 --run_times 20 --persistence naive 
+python example_benchmarks.py -d mfred --add_external_feature --observe_steps $observe_steps --window_width $window_width --gpu 0 --avg_terms_list 12 3 1 --run_times 20 --persistence loop 
 
 
 
